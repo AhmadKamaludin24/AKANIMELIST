6e4a77dda859da635ec045d136fb124bfdf10aea
AhmadKamaludin24 <150305598+AhmadKamaludin24@users.noreply.github.com>
2023-11-09 10:45:08 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    colors: {
+        color: {
+          dark: '#141414', 
+          accent: '#FF3A08',
+          secondary: '#F1EFEF'
+        }
+    }
+  },
+  plugins: [],
+}
