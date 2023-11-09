45963d4d27ef135f35c2088269f2a3514517592c
AhmadKamaludin24 <150305598+AhmadKamaludin24@users.noreply.github.com>
2023-11-09 11:05:41 +0700

    Add files via upload

---
+++ src/components/Utilities/HeaderMenu.js
@@ -0,0 +1,10 @@
+const HeaderMenu = ({title}) => {
+  return(
+    <div>
+      <div className="p-8">
+        <h3 className="text-2xl text-center text-color-secondary">{title}</h3>
+      </div>
+    </div>
+    )
+}
+export default HeaderMenu
---
+++ src/components/Utilities/Pagination.js
@@ -0,0 +1,30 @@
+const Pagination = ({ page, lastPage, setPage}) => {
+  const scrollTop = () => {
+    scrollTo({
+      behavior: "smooth", 
+      top: 0
+    })
+  }
+  
+  const handleNextPage = () => {
+    setPage((prevState) => prevState + 1)
+    scrollTop()
+  }
+  const handlePrevPage = () => {
+    setPage((prevState) => prevState - 1)
+    scrollTop()
+  }
+  return(
+    <div className="flex justify-center items-center py-8 px-4 gap-8 text-color-secondary text-2xl">
+    {page <= 1 ? null : 
+      <button onClick = {handlePrevPage} className='hover:text-color-accent'>Prev</button>
+    }
+      <p>{page} of {lastPage}</p>
+      {page >= lastPage ? null : 
+      <button onClick = {handleNextPage} className='hover:text-color-accent'>Next</button>
+        }
+      
+    </div>
+   )
+}
+export default Pagination
