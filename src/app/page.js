f1561d11baf5372cc865c3fbb06ba331d5a5f739
AhmadKamaludin24 <150305598+AhmadKamaludin24@users.noreply.github.com>
2024-02-27 12:10:36 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Header from "@/components/AnimeList/header"
 import { getAnimeResponse, getNestedAnimeResponse, reproduce} from "@/libs/api-libs"
 const Page = async () => {
     //const response = await fetch( `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
-    const topAnime = await getAnimeResponse("top/anime", "")
+    const topAnime = await getAnimeResponse("top/anime") 
     let recomendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
    recomendedAnime = reproduce(recomendedAnime, 10)
     
